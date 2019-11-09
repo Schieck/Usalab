044fe4f90a846e776326189e1a856f42523c4e5a
Schieck <ricardoschieck@gmail.com>
2019-11-09 19:13:28 -0200

    Add services and some components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router'; 
+//import { DashboardComponent } from './dashboard';
+import { LoginComponent } from './login';
+//import { RegisterComponent } from './register';
+//import { AuthGuard } from './_guards';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  //{ path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
