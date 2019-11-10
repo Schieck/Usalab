25950882057f6e08831f11f359d23dcd73b7f28f
Schieck <ricardoschieck@gmail.com>
2019-11-10 01:54:37 -0200

    Added login feature, registration and validations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'; 
-//import { DashboardComponent } from './dashboard';
-import { LoginComponent } from './login';
-//import { RegisterComponent } from './register';
-//import { AuthGuard } from './_guards';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component'; 
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
 const routes: Routes = [
-  { path: '', canActivate: [AuthGuard] },
+  { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-  //{ path: 'register', component: RegisterComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: '' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
-
 import { LoginComponent } from './login/login.component';
+
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SearchComponent } from './components/search/search.component';
+import { AlertComponent  } from './components/alert/alert.component';
+
+import { JwtInterceptor, ErrorInterceptor, fakeBackendProvider } from './helpers';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     SearchComponent,
-    LoginComponent
+    LoginComponent,
+    AlertComponent,
+    DashboardComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    MaterialModule,
+    ToastrModule.forRoot({
+      timeOut: 20000,
+      positionClass: 'toast-bottom-left',
+      preventDuplicates: true,
+      progressBar: true,
+      closeButton: true,
+      progressAnimation: "decreasing"
+    })
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
