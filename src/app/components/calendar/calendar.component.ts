490ce7b463537c666c16e4dfbc06011a1f8a1df9
Schieck <ricardoschieck@gmail.com>
2019-11-10 21:00:43 -0200

    adjusted versions

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -34,6 +34,7 @@ export class CalendarComponent implements OnInit {
       id:2
     },
   ];
+  
   horas = ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00']
 
   events: CalendarEvent[] = [];
