0f8f1dc984383c027e7e2985aff0c7bb39b33913
Bankow <lucas_bankow>
2019-11-10 11:37:55 -0600

    calendar

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -15,6 +15,16 @@ export class CalendarComponent implements OnInit {
 
   viewDate: Date = new Date();
   date = new Date();
+  obj = [ {
+    date: '01-01-1',
+    '09:00': {
+      hour: '09:00',
+      type: 'pesquisa',
+      agended: true,
+    }
+  }
+  ];
+  horas = ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00']
 
   events: CalendarEvent[] = [];
   constructor() { }
@@ -23,6 +33,16 @@ export class CalendarComponent implements OnInit {
   }
   value(params) {
     this.date = params;
+    let index =  params.getDate() + '-' + (params.getMonth() + 1) + '-' + params.getFullYear();
+    this.obj.push({
+      date: index,
+      '09:00': {
+        hour: '09:00',
+        type: 'pesquisa',
+        agended: true,
+      }
+    })
+    console.log(this.obj)
   }
 
 }
