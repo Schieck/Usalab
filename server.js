31d5bb397e865b8d4bbb8791e38887ab3927d7c4
Schieck <ricardoschieck@gmail.com>
2019-11-14 10:30:08 -0200

    adjusted app name

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/usalab'));
+app.use(express.static(__dirname + '/dist/usalab-app'));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/usalab/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/usalab-app/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
