e026eed8ccbdd59f3b2007efd932b466b92b8d99
Ricardo Schieck <ricardoschieck@gmail.com>
2019-11-14 11:40:28 -0200

    Adjusted app name.

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/Usalab/'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Usalab/index.html'));
 });
